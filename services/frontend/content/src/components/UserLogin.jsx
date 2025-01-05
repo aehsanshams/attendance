9503d7e7427304776ac1e8742a99c60b476ae076
Aehsan Shams <aehsanshams@gmail.com>
2025-01-05 19:21:22 +0530

    created a new code

--- services/frontend/content/src/components/UserLogin.jsx
+++ services/frontend/content/src/components/UserLogin.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from "react";
 
+
 function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -54,11 +55,6 @@ function LoginForm() {
       </div>
     );
   }
-  
-  function App() {
-    return (
-      <div>
-        <LoginForm />
-      </div>
-    );
-  }
+
+
+export default LoginForm; 

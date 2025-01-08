757f4aa0f5625daa26ba099a98c432a11a7cf96d
Aehsan Shams <aehsanshams@gmail.com>
2025-01-08 20:02:13 +0530

    added this file to Access-Control-Allow-Origin

---
+++ services/backend/html/cors.php
@@ -0,0 +1,18 @@
+<?php
+
+// Allow requests from any origin
+header("Access-Control-Allow-Origin: *");
+
+// Specify allowed HTTP methods
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+// Specify allowed headers
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// If this is a preflight request, exit early
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+

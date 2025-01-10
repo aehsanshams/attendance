c3f0cdccc073b3074eaf37dbcbd43d694f5af888
Aehsan Shams <aehsanshams@gmail.com>
2025-01-10 20:23:50 +0530

    remove some line

--- services/backend/html/logout.php
+++ services/backend/html/logout.php
@@ -1,11 +1,2 @@
 <?php
-require_once(__DIR__.'/cors.php');
 
-// Remove the token
-unset($tokens[$token]);
-
-// Save the updated tokens back to the file
-file_put_contents($token_file_path, json_encode($tokens));
-
-echo json_encode(['message' => 'Logout successful']);
-?>

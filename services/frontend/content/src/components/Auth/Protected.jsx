6e63117387ee7fefc1306d1e8e26ab4135accce7
Aehsan Shams <aehsanshams@gmail.com>
2025-01-10 21:50:07 +0530

    redirect to logIn page if user is not loggedin

--- services/frontend/content/src/components/Auth/Protected.jsx
+++ services/frontend/content/src/components/Auth/Protected.jsx
@@ -1,7 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { redirect, useNavigate } from "react-router";
+import api from 'axios';
 
 function Protected({Component}) {
     const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+
+    const getUser = async () => {
+        try{
+            const response = await api.get('http://localhost/profile.php');
+            console.log(response);
+            const data = response.data;
+            if (!data.loggedIn){
+                return navigate('/login');
+            }
+            setLoading(false);
+        }catch (error) {
+            console.log(error);
+            setErrorMessage('Registration was not successful');
+        }
+
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []);
 
     if(loading) {
         return (

0f472fca08aff238bfcbe897d01a767eb241da58
Aehsan Shams <aehsanshams@gmail.com>
2025-01-14 19:40:03 +0530

    token authorization

--- services/backend/html/libs/secured.php
+++ services/backend/html/libs/secured.php
@@ -1,13 +1,32 @@
 <?php
 
 $token = $_GET['token'];
-$token_directory = null;
-$token_file_name = null;
-$token_file_path = null;
-$token_file_content = null;
+$token_directory = __DIR__.'/..';
+$token_file_name = 'token.txt';
+$token_file_path = $token_directory.'/'.$token_file_name;
+$token_file_content = file_get_contents($token_file_path);
 
-$user_file = null;
+$lines = explode("\n", $token_file_content);
 
+//Find the line contians the user token
+$token_record = null;
+foreach($lines as $line){
+    if(strpos($line, $token . ':') === 0){
+        $token_record = $line;
+        break;
+    }
+}
+
+// If no token record means no data in our db/file for the user. Restrict the access
+if (!$token_record) {
+    die('Unauthorized access!');
+}
+
+//find the file path: right side of :(colon)
+// example value below: 
+// 77735654_1736834059:/var/www/html/data/aehsanshamsafgfgh5258yahoo.com.txt
+$file_path = (explode(":", $token_record));
+$user_file = $file_path[1];
 
 if (!$user_file) {
     die('Unauthorized access!');

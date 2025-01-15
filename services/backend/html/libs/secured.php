3a92f7d6438635570a324e2450d5e630db5ce05d
Aehsan Shams <aehsanshams@gmail.com>
2025-01-16 00:11:49 +0530

    function calling in secured file

--- services/backend/html/libs/secured.php
+++ services/backend/html/libs/secured.php
@@ -10,28 +10,14 @@ function getUserFile()
     $token_file_path = $token_directory . '/' . $token_file_name;
     $token_file_content = file_get_contents($token_file_path);
 
-    $lines = explode("\n", $token_file_content);
+    $tokens = stringToArray($token_file_content);
 
-    //Find the line contians the user token
-    $token_record = null;
-    foreach ($lines as $line) {
-        if (strpos($line, $token . ':') === 0) {
-            $token_record = $line;
-            break;
-        }
+    if (array_key_exists($token, $tokens)) {
+        $file_path = $tokens[$token];
+        return $file_path;
     }
 
-    // If no token record means no data in our db/file for the user. Restrict the access
-    if (!$token_record) {
-        die('Unauthorized access!');
-    }
-
-    //find the file path: right side of :(colon)
-    // example value below: 
-    // 77735654_1736834059:/var/www/html/data/aehsanshamsafgfgh5258yahoo.com.txt
-    $file_path = (explode(":", $token_record));
-    $user_file = $file_path[1];
-    return $user_file;
+    return null;
 }
 
 $user_file = getUserFile();

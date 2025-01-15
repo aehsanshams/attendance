e63574c71bd6a4389b12016edaf98a34e7da6a94
Aehsan Shams <aehsanshams@gmail.com>
2025-01-15 12:36:42 +0530

    edit the code

--- services/backend/html/table.php
+++ services/backend/html/table.php
@@ -1,17 +1,27 @@
 <?php
-$table = 2;
-$table_values =[];
-for($i=1; $i<=10; $i++)   
-{   
-//   echo $i*$table;   
-//   echo '<br>';  
-  $table_values[] =    $i*$table; 
-} 
+// $table = 2;
+// $table_values =[];
+// for($i=1; $i<=10; $i++)   
+// {   
+// //   echo $i*$table;   
+// //   echo '<br>';  
+//   $table_values[] =    $i*$table; 
+// } 
 
-// var_dump($table_values);
+// // var_dump($table_values);
 
-foreach($table_values as $value)
-{
-    echo ($value);
-    echo '<br>';
-}
+// foreach($table_values as $value)
+// {
+//     echo ($value);
+//     echo '<br>';
+// }
+
+// $str1 = 'Tomorrow I \'ll learn PHP global variables.';
+// $str2 = 'This is a bad command : del c:\\*.*';
+
+// echo $str1;
+// echo "<br />";
+// echo $str2;
+
+phpinfo();
+

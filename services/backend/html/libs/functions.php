529f67e6b33fac1e7e5e7c9909c6c0c35fbd67fc
Aehsan Shams <aehsanshams@gmail.com>
2025-01-15 23:51:19 +0530

    string to array and array to string cut to the secured file into function file

--- services/backend/html/libs/functions.php
+++ services/backend/html/libs/functions.php
@@ -1,3 +1,35 @@
 <?php
 
-function 
+/**
+ * input(array): [key1=>val1, key2=>value2, key_n=>value_n]
+ * 
+ * output(string):
+ * key1:val1\nkey2:value2\nkey_n:value_n
+ * 
+ */
+function arrayToString($array)
+{
+    $lines = '';
+    foreach($array as $key=>$value) {
+        $line = $key.':'.$value ."\n";
+        $lines = $lines.$line;
+    }
+    
+    return $lines;
+}
+
+/**
+ * input(string): key1:val1\nkey2:value2\nkey_n:value_n 
+ * outpu(array):  [key1=>val1, key2=>value2, key_n=>value_n]
+ */
+function stringToArray($string)
+{
+    $lines = explode("\n", $string);
+    $result = [];
+    foreach ($lines as $line) {
+        list($key, $value) = explode(":", $line);
+        $result[$key] = $value;
+    }
+
+    return $result;
+}
--- services/backend/html/libs/secured.php
+++ services/backend/html/libs/secured.php
@@ -1,41 +1,6 @@
 <?php
 
-/**
- * input(array): [key1=>val1, key2=>value2, key_n=>value_n]
- * 
- * output(string):
- * key1:val1\nkey2:value2\nkey_n:value_n
- * 
- */
-function arrayToString($array)
-{
-    $lines = '';
-    foreach($array as $key=>$value) {
-        $line = $key.':'.$value ."\n";
-        $lines = $lines.$line;
-    }
-    
-    return $lines;
-}
-
-/**
- * input(string): key1:val1\nkey2:value2\nkey_n:value_n 
- * outpu(array):  [key1=>val1, key2=>value2, key_n=>value_n]
- */
-function stringToArray($string)
-{
-    $lines = explode("\n", $string);
-    $result = [];
-    foreach ($lines as $line) {
-        list($key, $value) = explode(":", $line);
-        $result[$key] = $value;
-    }
-
-    return $result;
-}
-$result = "key1:val1\nkey2:value2\nkey_n:value_n";
-var_dump($result);
-die();
+include_once(__DIR__.'/functions.php');
 
 function getUserFile()
 {

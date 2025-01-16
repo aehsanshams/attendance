a3b6f28892358f68bd7642d3f8dfd2499b3c1382
Aehsan Shams <aehsanshams@gmail.com>
2025-01-16 20:43:54 +0530

    created associative array

--- services/backend/html/core-php/associativeArray.php
+++ services/backend/html/core-php/associativeArray.php
@@ -13,15 +13,15 @@
 
 // first method to  create an associative array.
 
-$studentName_one = array("Math"=>65, "Physics"=>70, "Chemistrey"=>68, "English"=>67, "Hindi"=>75);
+// $studentName_one = array("Math"=>65, "Physics"=>70, "Chemistrey"=>68, "English"=>67, "Hindi"=>75);
 
-// second method to create associative array
+// // second method to create associative array
 
-$studentName_two["Maths"] =  75;
-$studentName_two["Physics"] = 68;
-$studentName_two["Chemistrey"] = 70;
-$studentName_two["English"] = 76;
-$studentName_two["Hindi"] = 65;
+// $studentName_two["Maths"] =  75;
+// $studentName_two["Physics"] = 68;
+// $studentName_two["Chemistrey"] = 70;
+// $studentName_two["English"] = 76;
+// $studentName_two["Hindi"] = 65;
 
 // Accessing the elements directly 
 
@@ -36,15 +36,35 @@ $studentName_two["Hindi"] = 65;
 
 // looping through foreach
 
-echo "looping using foreach: \n\n";
-foreach($studentName_one as $subject => $marks) {
-    echo "Student one got " .$marks. " in " .$subject."\n";
-}
+// echo "looping using foreach: \n\n";
+// foreach($studentName_one as $subject => $marks) {
+//     echo "Student one got " .$marks. " in " .$subject."\n";
+// }
 
-echo "\nlooping using for: \n\n";
-$subject = array_keys($studentName_two);
-$marks = count($studentName_two);
+// echo "\nlooping using for: \n\n";
+// $subject = array_keys($studentName_two);
+// $marks = count($studentName_two);
 
-for($i=0; $i<$marks; ++$i) {
-    echo $subject[$i] . ' ' . $studentName_two[$subject[$i]] . "\n";
+// for($i=0; $i<$marks; ++$i) {
+//     echo $subject[$i] . ' ' . $studentName_two[$subject[$i]] . "\n";
+// }
+
+// Array Multi_short 
+
+// Declearing an associative array.
+$arr = array(
+    array("name"=>"Hassan", "Age"=>23),
+    array("name"=>"Zishan", "Age"=>22),
+    array("name"=>"salman", "Age"=>18),
+    array("name"=>"Nurain", "Age"=>20)
+);
+// Declearing an array to store names
+$names = array();
+// Iterating over the array
+foreach($arr as $key => $val) {
+    // storing the key of the names array as the Name key of the arr
+    $names[$key] = $val['name'];
 }
+array_multisort($names, SORT_ASC, $arr);
+print_r("Modified array : ");
+print_r($arr);

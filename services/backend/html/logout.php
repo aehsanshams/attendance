b08566737ea4269783ef90299c5274a804629ad3
Aehsan Shams <aehsanshams@gmail.com>
2025-01-20 16:13:10 +0530

    created logout function in php

--- services/backend/html/logout.php
+++ services/backend/html/logout.php
@@ -1,5 +1,13 @@
 <?php
 require_once(__DIR__.'/libs/cors.php');
-// require_once(__DIR__.'/libs/secured.php');
+require_once(__DIR__.'/libs/secured.php');
 
+$token_file_path = __DIR__.'/token.txt';
+
+if (file_exists($token_file_path)) {
+    unlink($token_file_path); // Deletes the token file
+    echo 'Logged out successfully';
+} else {
+    echo 'No active session found';
+}
 

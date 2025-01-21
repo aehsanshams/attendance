fddd3befc1fda970428a19517dbd1fd08dc02592
Aehsan Shams <aehsanshams@gmail.com>
2025-01-21 20:18:51 +0530

    added bootstrap and created navbar

--- services/frontend/content/src/components/HomePage.jsx
+++ services/frontend/content/src/components/HomePage.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import api from "axios";
 
+
+// import Navbar from "./Navbar";
+
 function HomePage() {
   const [loading, setLoading] = useState(true);
   const [content, setContent] = useState("");
@@ -23,10 +26,38 @@ function HomePage() {
   }
 
   return (
-    <div style={{ maxWidth: "400px", margin: "auto", padding: "20px", textAlign: "center" }}>
-      <h2>{content}</h2>
-      <h3>Welcome to Home page</h3>
-    </div>
+    <>
+
+<div className="container">
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Home</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Attendance</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+</div>
+      <div style={{ maxWidth: "400px", margin: "auto", padding: "20px", textAlign: "center" }}>
+        <h1>Welcome to Home page</h1>
+        <h2>below is server content:</h2>
+        {content}
+      </div>
+
+    </>
   );
 }
 

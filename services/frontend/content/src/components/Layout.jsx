bba081796f8d24312b344a49e59f6fdd675b7479
Aehsan Shams <aehsanshams@gmail.com>
2025-01-22 17:59:53 +0530

    styled navbar

--- services/frontend/content/src/components/Layout.jsx
+++ services/frontend/content/src/components/Layout.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { NavLink } from 'react-router';
 
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: 'black'
+};
+
 function Layout({ children }) {
 
     return (
@@ -11,12 +17,7 @@ function Layout({ children }) {
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
                         {/* <a className="navbar-brand" href="#">Home</a> */}
-                        <NavLink to="/" end style={({ isActive, isPending }) => {
-                            return {
-                                fontWeight: isActive ? "bold" : "",
-                                color: isPending ? "red" : "black",
-                            };
-                        }}>Home</NavLink>
+                        <NavLink to="/" end style={linkStyle}>Home</NavLink>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -24,7 +25,7 @@ function Layout({ children }) {
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
                                     {/* <a className="nav-link active" aria-current="page" href="">Attendance</a> */}
-                                    <NavLink to="/attendance" end>Attendance</NavLink>
+                                    <NavLink to="/attendance" end style={linkStyle}>Attendance</NavLink>
 
                                 </li>
                             </ul>

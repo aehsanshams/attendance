570b0f816d851c18b7c2cc3b49f1d9d4c9d819dd
Aehsan Shams <aehsanshams@gmail.com>
2025-01-23 00:36:42 +0530

    services/frontend/content/src/components/Attendance.jsx

--- services/frontend/content/src/components/Attendance.jsx
+++ services/frontend/content/src/components/Attendance.jsx
@@ -2,13 +2,18 @@ import React from "react";
 
 
 function Attendance() {
+    clock
 
     return (
         <>
         <div style={{ Width: "100%", margin: "150px", padding: "20px", textAlign: "center" }}>
         <h1>Welcome to Attendance page</h1>
+      
+      date : <input type="text" /> <br />
+      time : <input type="text" /> <br />
+      <input type="submit" value="Clock In"/>
       </div>
-        </>
+      </>
     );
 }
 

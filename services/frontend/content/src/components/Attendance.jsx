1335dc9ec8908f5e6b9b05767e1f2ab120f00154
Aehsan Shams <aehsanshams@gmail.com>
2025-01-23 17:29:06 +0530

    created attendance.php file

--- services/frontend/content/src/components/Attendance.jsx
+++ services/frontend/content/src/components/Attendance.jsx
@@ -1,35 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import api from 'axios';
 
-function Attendance() {
+const Loading = () => (<>Loading...</>);
 
-  const [clockIn, setClockIn] = useState('');
-  const [clockOut, setClockOut] = useState('');
-  const [massege, setMassege] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if(!clockIn) {
-      massege('please clocked-in');
-      return
-    }else{
-      massage('You are clocked-in');
-      return
-    }
-  }
-    return (
-        <>
-        <div style={{ Width: "100%", margin: "150px", padding: "20px", textAlign: "center" }}>
+const ClockInForm = () => {
+  return (
+    <>
+      <div style={{ Width: "100%", margin: "150px", padding: "20px", textAlign: "center" }}>
         {/* <h1>Welcome to Attendance page</h1> */}
-      
-        <h1><label for="clock-in">User Attendance</label></h1><br />
-        <input type="datetime-local" name="date-time" onChange={(e) => setClockIn(e.target.value)}/><br /><br />
-        <button onClick={handleSubmit} type="submit" value="Clock-In">Clock-In</button>
-        
+
+        {/* <h1><label for="clock-in">{massege}</label></h1><br />
+        <input type="datetime-local" name="date-time" onChange={(e) => setClockIn(e.target.value)} /><br /><br />
+        <button onClick={handleSubmit} type="submit" value="Clock-In">Clock-In</button> */}
+        Clock in form here
+
 
       </div>
-      </>
-    );
+    </>
+  );
+}
+
+const ClockOutForm = () => {
+  return (
+    <>
+      Clock out form
+    </>
+  );
+}
+
+
+function Attendance() {
+  const [isClockedId, setIsClockedIn] = useState(true);
+  // const [attendanceData, setAttendanceData] = useState({ clockInTime: null, clockOutTime: null });
+  // const [massege, setMassege] = useState('');
+
+  useEffect(() => {
+    const checkIsClockedIn = () => {
+
+    }
+    checkIsClockedIn();
+  }, []);
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   if (!clockIn) {
+  //     setMassege('Please clock-in');
+  //     return;
+  //   }
+  //   const reqData = {
+  //     clockIn,
+  //   };
+  //   api.post('http://localhost/attendance.php', reqData)
+  //     .then(function (response) {
+  //       console.log(response);
+  //       alert(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
+
+  if (isClockedId === null) {
+    return <Loading />
+  }
+
+  if (isClockedId) {
+    return <ClockOutForm/>
+  }
+
+  return <ClockInForm/>;
 }
 
 export default Attendance;

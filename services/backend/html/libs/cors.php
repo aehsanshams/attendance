a8c5eb9ab88fe4c42029e1d82128bf782cafd3ee
Aehsan Shams <aehsanshams@gmail.com>
2025-01-23 10:56:18 +0530

    created attendance sheet

--- services/backend/html/libs/cors.php
+++ services/backend/html/libs/cors.php
@@ -9,6 +9,8 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 // Specify allowed headers
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
+header("Content-Type: application/json");
+
 // If this is a preflight request, exit early
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);

23f36d106753fda82ccc622f93cfcde99211aeb9
Aehsan Shams <aehsanshams@gmail.com>
2025-01-27 22:25:48 +0530

    some changes

--- services/frontend/content/src/components/TextHighlighter.jsx
+++ services/frontend/content/src/components/TextHighlighter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 function TextHilighter() {
-    const [input, setInput] = useState('abcdefghijklmn');
-    const [position, setPosition] = useState(11);
+    const [input, setInput] = useState(' ');
+    const [position, setPosition] = useState(null);
 
     const [textHighlighted, setTextHighlighted] = useState(<></>);
 
@@ -12,22 +12,23 @@ function TextHilighter() {
 
         const before = input.substring(0, position-1)
         // abcdefghij ; 0, 10
-        console.log('startP:', 0, 'endP:', position-1);
-        console.log('before: ', before);
+        // console.log('startP:', 0, 'endP:', position-1);
+        // console.log('before: ', before);
 
         let center = input.substring(position-1, position);
         // k; 10,10
-        console.log('startP:', position-1, 'endP:', position);
-        console.log('center: ', center);
+        // console.log('startP:', position-1, 'endP:', position);
+        // console.log('center: ', center);
+
         center = (<span style={{backgroundColor:'yellow'}}>{center} </span>);
-        console.log(center);
+        // console.log(center);
 
         const after = input.substring(position, input.length);
         // lmn; 11, 13
-        console.log('startP:', position, 'endP:', input.length);
-        console.log('after: ', after);
+        // console.log('startP:', position, 'endP:', input.length);
+        // console.log('after: ', after);
 
-        const result = <>{before}{center} {after}</>;
+        const result = <>{before}{center}{after}</>;
         setTextHighlighted(result);
 
 
@@ -54,7 +55,6 @@ function TextHilighter() {
                 </div>
                 <div>
                     <label htmlFor="integerInput" className="block mb-2 font-medium text-gray-700">
-                        Position
                     </label>
                     <input
                         id="position"
@@ -67,11 +67,12 @@ function TextHilighter() {
                 <div>
                     <button onClick={highlight}>Hilight</button>
                 </div>
-                <div>
-                    <label htmlFor="result"></label>
-
-                        {textHighlighted}
+                <div className="mt-6">
+                <label className="block mb-2 font-medium text-gray-700">Result:</label>
+                <div className="border border-gray-300 rounded p-2 w-96 bg-white">
+                    {textHighlighted}
                 </div>
+            </div>
             </div>
 
         </>
